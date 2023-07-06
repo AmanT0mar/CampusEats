59d3cec026cfa7f1efbde5f0be531c930dcc4730
AmanT0mar <amantomar123456789@gmail.com>
2023-07-06 11:18:58 +0530

    CampusEats

--- app/src/main/java/com/example/campuseats/RecyclerViewAdapterCart.java
+++ app/src/main/java/com/example/campuseats/RecyclerViewAdapterCart.java
@@ -12,10 +12,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.campuseats.viewmodel.CartItems;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
